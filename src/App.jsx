6c68c7046ca3e2fdeaad759e9c841a2a56c78b26
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-03-04 01:34:33 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GSAP from "gsap";
+
+import Card from "./card";
+
+export default function App() {
+  const handleMouseMove = (e) => {
+    console.log(e);
+    GSAP.to(".cursor", {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 3,
+      ease: "back.out",
+    });
+  };
+  return (
+    <div onMouseMove={handleMouseMove} className="world">
+      <img
+        className="cursor"
+        src="https://png.pngtree.com/png-vector/20230418/ourmid/pngtree-donut-afternoon-tea-dessert-shop-transparent-png-image_6708371.png"
+        alt="donut"
+      />
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
