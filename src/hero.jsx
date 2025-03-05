5304113bc3c0645ad75c5b4e4b8cba00ac9193fe
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-03-06 00:46:14 +0530

    timely commit

--- src/hero.jsx
+++ src/hero.jsx
@@ -2,6 +2,16 @@ import React, { useRef } from "react";
 import "./index.css";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import {
+  Github,
+  Linkedin,
+  Mail,
+  ExternalLink,
+  Code2,
+  Layout,
+  Database,
+  Terminal,
+} from "lucide-react";
 
 export default function Hero() {
   const cursorRef = useRef();
@@ -19,6 +29,17 @@ export default function Hero() {
         <div className="glass"></div>
         <div ref={cursorRef} className="hero-cursor"></div>
         <div className="info">
+          <div className="links">
+            <a href="https://github.com/shreyasgadhe">
+              <Github color="antiquewhite" />
+            </a>
+            <a href="https://www.linkedin.com/in/shreyasgadhe/">
+              <Linkedin color="antiquewhite" />
+            </a>
+            <a href="mailto:gadheshreyas@gmail.com">
+              <Mail color="antiquewhite" />
+            </a>
+          </div>
           <div className="pfp"></div>
           <h1>SHREYAS GADHE</h1>
           <p className="desc">Front-End Engineer</p>
