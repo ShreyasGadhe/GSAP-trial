5a8f3cd4e51b4937fd8eccf7b92517a232452070
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-03-05 23:51:53 +0530

    tried adding random logo animations

--- src/hero.jsx
+++ src/hero.jsx
@@ -1,9 +1,11 @@
 import React, { useRef } from "react";
 import "./index.css";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Hero() {
   const cursorRef = useRef();
+  const reactLogoRef = useRef();
 
   const handleMouseMove = (e) => {
     gsap.to(cursorRef.current, {
@@ -12,8 +14,26 @@ export default function Hero() {
     });
   };
 
+  useGSAP(() => {
+    gsap.from(reactLogoRef.current, {
+      left: 0,
+      duration: 10,
+      delay: 0.5,
+      ease: "power3.inOut",
+      repeat: 2,
+      rotation: 360,
+      repeatDelay: 1,
+      yoyo: true,
+    });
+  }, []);
+
   return (
     <>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+        className="react-logo"
+        ref={reactLogoRef}
+      />
       <div onMouseMove={handleMouseMove} className="hero">
         <div className="glass"></div>
         <div ref={cursorRef} className="hero-cursor"></div>
