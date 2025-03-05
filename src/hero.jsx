7ce87458f7bb0c61cccf250e076f55c057f1efde
Shreyas Gadhe <gadheshreyas@gmail.com>
2025-03-06 00:18:07 +0530

    updated her section

--- src/hero.jsx
+++ src/hero.jsx
@@ -5,7 +5,6 @@ import { useGSAP } from "@gsap/react";
 
 export default function Hero() {
   const cursorRef = useRef();
-  const reactLogoRef = useRef();
 
   const handleMouseMove = (e) => {
     gsap.to(cursorRef.current, {
@@ -14,30 +13,16 @@ export default function Hero() {
     });
   };
 
-  useGSAP(() => {
-    gsap.from(reactLogoRef.current, {
-      left: 0,
-      duration: 10,
-      delay: 0.5,
-      ease: "power3.inOut",
-      repeat: 2,
-      rotation: 360,
-      repeatDelay: 1,
-      yoyo: true,
-    });
-  }, []);
-
   return (
     <>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
-        className="react-logo"
-        ref={reactLogoRef}
-      />
       <div onMouseMove={handleMouseMove} className="hero">
         <div className="glass"></div>
         <div ref={cursorRef} className="hero-cursor"></div>
-        <h1>HELLO WORLD</h1>
+        <div className="info">
+          <div className="pfp"></div>
+          <h1>SHREYAS GADHE</h1>
+          <p className="desc">Front-End Engineer</p>
+        </div>
       </div>
     </>
   );
